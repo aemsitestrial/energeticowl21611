92ff9e84d5098cdbc1bcfaca20126685a85dcacb
Naveenkumar S <2963078@tcs.com>
2026-09-23 14:59:11 +0530

    resetting the fragment to initial

--- blocks/fragment/fragment.js
+++ blocks/fragment/fragment.js
@@ -1,6 +1,7 @@
 /*
  * Fragment Block
  * Include content on a page as a fragment.
+ * https://www.aem.live/developer/block-collection/fragment
  */
 
 import {
@@ -20,64 +21,38 @@ export async function loadFragment(path) {
   if (path && path.startsWith('/')) {
     // eslint-disable-next-line no-param-reassign
     path = path.replace(/(\.plain)?\.html/, '');
-
     const resp = await fetch(`${path}.plain.html`);
-
     if (resp.ok) {
       const main = document.createElement('main');
-
       main.innerHTML = await resp.text();
 
+      // reset base path for media to fragment base
       const resetAttributeBase = (tag, attr) => {
         main.querySelectorAll(`${tag}[${attr}^="./media_"]`).forEach((elem) => {
-          elem[attr] = new URL(
-            elem.getAttribute(attr),
-            new URL(path, window.location),
-          ).href;
+          elem[attr] = new URL(elem.getAttribute(attr), new URL(path, window.location)).href;
         });
       };
-
       resetAttributeBase('img', 'src');
       resetAttributeBase('source', 'srcset');
 
       decorateMain(main);
       await loadSections(main);
-
       return main;
     }
   }
-
   return null;
 }
 
 export default async function decorate(block) {
-  const rows = [...block.children];
-
-  const fragmentType = rows[0]?.textContent?.trim() || 'standard';
-
   const link = block.querySelector('a');
-
-  const path = link
-    ? link.getAttribute('href')
-    : block.textContent.trim();
-
-  switch (fragmentType) {
-    case 'standard':
-    default: {
-      const fragment = await loadFragment(path);
-
-      if (fragment) {
-        const fragmentSection = fragment.querySelector(':scope .section');
-
-        if (fragmentSection) {
-          block.classList.add(...fragmentSection.classList);
-          block.classList.remove('section');
-
-          block.replaceChildren(...fragmentSection.childNodes);
-        }
-      }
-
-      break;
+  const path = link ? link.getAttribute('href') : block.textContent.trim();
+  const fragment = await loadFragment(path);
+  if (fragment) {
+    const fragmentSection = fragment.querySelector(':scope .section');
+    if (fragmentSection) {
+      block.classList.add(...fragmentSection.classList);
+      block.classList.remove('section');
+      block.replaceChildren(...fragmentSection.childNodes);
     }
   }
 }
